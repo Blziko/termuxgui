117d32b80f07a1fa7bb2caec775bb1e474a9e3df
Blziko <reinabee8443@gmail.com>
2026-04-29 15:48:23 +0700

    modified: autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -19,8 +19,6 @@ require=(
     "virglrenderer-android"
     "chromium"
     "florence"
-    "adwaita-icon-theme"
-    "papirus-icon-theme"
     "code-is-code-oss"
     "pulseaudio"
     "audacious"
@@ -29,13 +27,15 @@ require=(
     "xwayland"
     "mousepad"
     "geany"
-    "synaptic"
+    "kate"
     "galculator"
     "gtk-update-icon-cache"
     "scdaemon"
     "mesa-dev"
     "plank-reloaded"
     "gimp"
+    "konsole"
+    "kdenlive"
 )
 
 command apt install -y "${require[@]}"
@@ -45,24 +45,6 @@ echo -e \
     '[Settings]\ngtk-application-prefer-dark-theme=1' \
     > "${HOME}/.config/gtk-3.0/settings.ini"
 
-command mkdir -pv "${HOME}/.local/share/fonts"
-cd "${HOME}/.local/share/fonts"
-command curl -fLO \
-    'https://github.com/ryanoasis/nerd-fonts/releases/latest/download/SourceCodePro.zip'
-command unzip 'SourceCodePro.zip'
-command rm -fv SourceCodePro.zip
-command fc-cache -fv
-cd
-
-command mkdir -pv "${HOME}/.icons"
-command git clone --depth 1 \
-    'https://github.com/vinceliuice/WhiteSur-icon-theme.git' \
-    "${HOME}/.icons/WhiteSur-icon-theme"
-cd "${HOME}/.icons/WhiteSur-icon-theme"
-command chmod +x install.sh
-./install.sh
-cd
-
 command git clone --depth 1 \
     'https://github.com/Zeronetsec/Muxly.git'
 cd Muxly
@@ -70,16 +52,14 @@ command chmod +x install.sh
 ./install.sh
 cd
 
-command muxly --volume-keys volume
-command muxly --chfont source_code_pro
-command muxly --reset-theme
-command muxly --chcursor underline
-command muxly --chcursor-blink-rate 500
-command muxly --shortcut-create-session 'ctrl + `'
-command muxly --shortcut-next-session 'ctrl + >'
-command muxly --shortcut-previous-session 'ctrl + <'
-command muxly --shortcut-rename-session 'ctrl + :'
 command muxly --install-rootfs kali_rolling
+
+command mkdir -pv "${HOME}/.wallpaper"
+command mkdir -pv "${HOME}/.icons"
+command mkdir -pv "${HOME}/.themes"
+command mkdir -pv "${HOME}/.config"
+command mkdir -pv "${HOME}/.local/share/fonts"
+command mkdir -pv "${HOME}/.config/audacious"
 
 if [[ ! -d "${HOME}/storage" ]] && ! command ls '/sdcard/' >/dev/null 2>&1; then
     command termux-setup-storage
